d5a26087f1ab137c6fd2c2617fc2d3fe29439bd9
Gerald <geraldonod@gmail.com>
2024-02-08 16:47:19 +0800

    Added Python Imports and Game Constants/Settings

--- SNAKE GAME.py
+++ SNAKE GAME.py
@@ -1,5 +1,18 @@
 #Imports and Constants
 
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 80
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+direction = 'down'
+
 #Snake Class
 
 #Food Class
