3f088a0106b998d6c92844b1c6c8b74ca9c89a83
Gerald <geraldonod@gmail.com>
2024-02-09 15:48:01 +0800

    Added Snake Class

--- SNAKE GAME.py
+++ SNAKE GAME.py
@@ -15,6 +15,21 @@ direction = 'down'
 
 #Snake Class
 
+class Snake:
+
+    def __init__(self, canvas):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+        self.canvas = canvas
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = self.canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+
 #Food Class
 
 class Food:
