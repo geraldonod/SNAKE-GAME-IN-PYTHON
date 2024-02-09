ac463e9b64748ccd0420a6341b3e9198f64432d6
Gerald <geraldonod@gmail.com>
2024-02-09 14:44:22 +0800

    Added Main Execution

--- SNAKE GAME.py
+++ SNAKE GAME.py
@@ -24,4 +24,39 @@ direction = 'down'
 #Window Setup
 
 #Main Execution
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+score = 0
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
+snake = Snake(canvas)
+food = Food(canvas)
+
+next_turn(snake, food)
+
+window.mainloop()
 
