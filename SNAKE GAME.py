2edef79a46ccc49905e3138ddf301012ddc32c0c
Gerald <geraldonod@gmail.com>
2024-02-09 17:17:44 +0800

    Added check_collisions Function

--- SNAKE GAME.py
+++ SNAKE GAME.py
@@ -108,7 +108,22 @@ def change_direction(new_direction):
     elif new_direction == 'down':
         if direction != 'up':
             direction = new_direction
-            
+
+def check_collisions(snake):
+
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
+    
 #User Input Handling
 
 #Window Setup
