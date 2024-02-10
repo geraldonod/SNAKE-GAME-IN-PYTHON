0a4f7644648ddfafb717a94aeefcbcabaebd4995
Gerald <geraldonod@gmail.com>
2024-02-10 14:34:56 +0800

    Finishing Touches

--- SNAKE GAME.py
+++ SNAKE GAME.py
@@ -3,14 +3,14 @@
 from tkinter import *
 import random
 
-GAME_WIDTH = 700
-GAME_HEIGHT = 700
-SPEED = 80
+GAME_WIDTH = 800
+GAME_HEIGHT = 800
+SPEED = 90
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF0000"
-BACKGROUND_COLOR = "#000000"
+SNAKE_COLOR = "#6CBB3C"
+FOOD_COLOR = "#DC381F"
+BACKGROUND_COLOR = "#2B65EC"
 direction = 'down'
 
 #Snake Class
@@ -91,7 +91,6 @@ def next_turn(snake, food):
     else:
         window.after(SPEED, next_turn, snake, food)
 
-
 def change_direction(new_direction):
 
     global direction
@@ -132,7 +131,7 @@ def game_over():
 
 #Main Execution
 window = Tk()
-window.title("Snake Game")
+window.title("Sea Snake Adventure")
 window.resizable(False, False)
 
 score = 0
